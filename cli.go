a53a8b4a81ee836d75c034f5105e5f4789f9e1a4
indedhat <iam4423@gmail.com>
2022-05-10 15:54:11 +0100

    fix: wrapped CliUsage function to make it compatible with flags

--- cli.go
+++ cli.go
@@ -34,49 +34,51 @@ func (ces CliCommandEntries) Find(key string) *CliCommandEntry {
 }
 
 // CliUsage generator for the flags lib
-func CliUsage(title, description, binName string, register CliCommandEntries) {
-	var builder *bytes.Buffer
+func CliUsage(title, description, binName string, register CliCommandEntries) func() {
+	return func() {
+		var builder *bytes.Buffer
 
-	builder.WriteString(title)
-	builder.WriteByte('\n')
-
-	if description != "" {
-		builder.WriteString(description)
+		builder.WriteString(title)
 		builder.WriteByte('\n')
-		builder.WriteByte('\n')
-	}
 
-	if binName == "" {
-		binName = "server"
-	}
-
-	builder.WriteString(fmt.Sprintf("USAGE:\n    ./%s [options]\n\n", binName))
-	builder.WriteString("OPTIONS:\n")
-
-	fmt.Print(builder.String())
-	flag.PrintDefaults()
-
-	fmt.Print("\nCOMMANDS:\n")
-
-	green := color.New(color.FgGreen).SprintFunc()
-	yellow := color.New(color.FgYellow).SprintFunc()
-
-	rows := make([]string, 0, len(register))
-	for _, cmd := range register {
-		for i, line := range strings.Split(cmd.Usage, "\n") {
-			key := cmd.Key
-			if i != 0 {
-				key = "..."
-			}
-
-			rows = append(rows, fmt.Sprintf("    %s\t%s\n", green(key), yellow(line)))
+		if description != "" {
+			builder.WriteString(description)
+			builder.WriteByte('\n')
+			builder.WriteByte('\n')
 		}
-	}
 
-	tbl := tabwriter.NewWriter(os.Stdout, 0, 4, 4, ' ', tabwriter.StripEscape)
-	for _, row := range rows {
-		fmt.Fprint(tbl, row)
-	}
+		if binName == "" {
+			binName = "server"
+		}
 
-	tbl.Flush()
+		builder.WriteString(fmt.Sprintf("USAGE:\n    ./%s [options]\n\n", binName))
+		builder.WriteString("OPTIONS:\n")
+
+		fmt.Print(builder.String())
+		flag.PrintDefaults()
+
+		fmt.Print("\nCOMMANDS:\n")
+
+		green := color.New(color.FgGreen).SprintFunc()
+		yellow := color.New(color.FgYellow).SprintFunc()
+
+		rows := make([]string, 0, len(register))
+		for _, cmd := range register {
+			for i, line := range strings.Split(cmd.Usage, "\n") {
+				key := cmd.Key
+				if i != 0 {
+					key = "..."
+				}
+
+				rows = append(rows, fmt.Sprintf("    %s\t%s\n", green(key), yellow(line)))
+			}
+		}
+
+		tbl := tabwriter.NewWriter(os.Stdout, 0, 4, 4, ' ', tabwriter.StripEscape)
+		for _, row := range rows {
+			fmt.Fprint(tbl, row)
+		}
+
+		tbl.Flush()
+	}
 }
